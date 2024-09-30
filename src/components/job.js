74171b6c583295f0057732d9be1c44bcd0a724f6
Jaime Torres <156240086+mews6@users.noreply.github.com>
2024-09-29 21:54:35 -0500

    final commit

--- src/components/job.js
+++ src/components/job.js
@@ -1,14 +1,60 @@
 import React from "react";
-import {FormattedDate} from 'react-intl';
+import {FormattedDate, FormattedMessage, FormattedNumber} from 'react-intl';
 
 const Job = (props) => {
+
+  function millionCheck() {
+    console.log(navigator.language)
+    if (navigator.language !== 'es-ES') {
+      return (
+        <FormattedMessage
+        id="PositionSalary"
+        values={{value:props.offer.salary}}
+        />
+      )
+    }
+    if (props.offer.salary === 1) {
+      return (
+        <FormattedMessage
+        id="1MSalary"
+        values={{value:props.offer.salary}}
+      />
+      );
+    } else {
+      return (
+        <FormattedMessage
+        id="DiffSalary"
+        values={{value:props.offer.salary}}
+      />        
+      )
+    }
+  }
+
   return (
     <tr>
       <th scope="row">{props.offer.id}</th>
-      <td>{props.offer.name}</td>
-      <td>{props.offer.company}</td>
-      <td>{props.offer.salary}</td>
-      <td>{props.offer.city}</td>
+      
+      <td><FormattedMessage
+      id="GenericString"
+      values={{value:props.offer.name}}
+      />
+      </td>
+      
+      <td><FormattedMessage
+      id="GenericString"
+      values={{value:props.offer.company}}
+      /></td>
+      
+      <td>
+      {millionCheck()}
+      </td>
+      
+      <td><FormattedMessage
+      id="GenericString"
+      values={{value:props.offer.city}}
+      />
+      </td>
+      
       <td>
         <FormattedDate
           value={new Date(props.offer.date)}
@@ -18,6 +64,13 @@ const Job = (props) => {
           weekday='long'
         />
       </td>
+      
+      <td>
+        <FormattedNumber
+        value={props.offer.views}
+        />
+      </td>
+      
     </tr>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {IntlProvider} from 'react-intl';
 import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 import JobsList from "./components/jobslist";
 
+function checkLang() {
+    if (navigator.language === "es-ES") {
+        return (localeEsMessages)
+    } else {
+        return (localeEnMessages)
+    }
+}
+
 ReactDOM.render(
-    <IntlProvider locale="es-ES" messages= {localeEsMessages}>
+    <IntlProvider locale={navigator.language} messages= {checkLang()}>
             <JobsList/>
     </IntlProvider>, document.getElementById("root")
 );
